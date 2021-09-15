dc0980c06daa1dafb69ee525884e41739503ece8
rusthigh <rusthigh@proton.me>
2021-09-15 14:53:47 +0000

    KualiCo Release Notes and Scripts #norelease #nobuild

--- app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
+++ app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
@@ -2,4 +2,9 @@ package com.example.crypto.adapters
 
 import android.view.LayoutInflater
 import android.view.View
-import android.
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.crypto.R
+import com.example.crypto.fragment.CoinPricefragment1
+import com.example.crypto.pojo.CoinPric
