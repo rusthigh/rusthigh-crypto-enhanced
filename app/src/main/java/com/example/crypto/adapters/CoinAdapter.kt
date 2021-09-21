2a781b965174b8367ca74a269b692e455c01e99b
rusthigh <rusthigh@proton.me>
2021-09-21 05:44:34 +0000

    Added readme

--- app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
+++ app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
@@ -7,4 +7,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.crypto.R
 import com.example.crypto.fragment.CoinPricefragment1
-import com.example.crypto.pojo.CoinPric
+import com.example.crypto.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.item_coin_info.view.*
+
+
+class CoinAdapter(private val context: CoinPricefragment1) :
+    RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+    var coinInfoList: List<CoinPriceInf
