d3e101dc2cd7f5a2e35cd0c012556f7c2c6f24c5
rusthigh <rusthigh@proton.me>
2021-10-10 14:56:40 +0000

    Bump to 1.8.0

--- app/src/main/java/com/example/crypto/database/AppDatabase.kt
+++ app/src/main/java/com/example/crypto/database/AppDatabase.kt
@@ -1,3 +1,9 @@
 package com.example.crypto.database
 
-import android
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.crypto.pojo.CoinPriceInfo
+
+@Database(entities = [CoinPriceInfo::class], vers
