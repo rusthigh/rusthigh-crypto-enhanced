032a044d1b9768155ed0f8074bb6bdeb3be215df
rusthigh <rusthigh@proton.me>
2021-10-10 16:44:26 +0000

    Updated the version

--- app/src/main/java/com/example/crypto/database/AppDatabase.kt
+++ app/src/main/java/com/example/crypto/database/AppDatabase.kt
@@ -6,4 +6,10 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.crypto.pojo.CoinPriceInfo
 
-@Database(entities = [CoinPriceInfo::class], vers
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+
+        private var db: AppDatabase? = null
+        private const val DB_NAME = "main.db"
+        private val LOCK =
