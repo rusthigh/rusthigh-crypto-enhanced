eb96196a61289cdde2645de05ca1efd40440ef9b
rusthigh <rusthigh@proton.me>
2021-10-26 11:55:03 +0000

    (CT) updated code

--- app/src/main/java/com/example/crypto/database/CoinPriceDAO.kt
+++ app/src/main/java/com/example/crypto/database/CoinPriceDAO.kt
@@ -1,1 +1,11 @@
-package com.example.crypto.dat
+package com.example.crypto.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.crypto.pojo.CoinPriceInfo
+
+@Dao
+in
