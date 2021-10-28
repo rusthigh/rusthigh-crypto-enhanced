357b1318f77ac48a9dbeccbff3a1bdc8dc585f95
rusthigh <rusthigh@proton.me>
2021-10-28 09:30:50 +0000

    md/raid5: don't record new size if resize_stripes fails.

--- app/src/main/java/com/example/crypto/fragment/CoinPricefragment1.kt
+++ app/src/main/java/com/example/crypto/fragment/CoinPricefragment1.kt
@@ -1,1 +1,9 @@
-package com.example.crypto.fragme
+package com.example.crypto.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
