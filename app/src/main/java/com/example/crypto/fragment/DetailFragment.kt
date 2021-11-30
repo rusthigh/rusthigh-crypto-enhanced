a9a123e620cf8041a4a1367e12ab429636e46b64
rusthigh <rusthigh@proton.me>
2021-11-30 07:43:15 +0000

    Update command.js

--- app/src/main/java/com/example/crypto/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/crypto/fragment/DetailFragment.kt
@@ -5,4 +5,10 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.example.crypto.CoinViewModel
+import
